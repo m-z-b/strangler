3174410f4e2f28ee7d42e333b19fefa250613915
Michael Bell <mbell@albionresearch.com>
2018-01-17 14:57:09 -0500

    Refactor testing; remove unused module

---
+++ lib/throttler.rb
@@ -0,0 +1,32 @@
+require 'thread'
+
+# Limit calls to an API by sleeping
+class Throttler
+  VERSION = "0.1.0"
+
+
+  # Set the minimum delay between calls
+  def initialize( minimum_delay_secs = 1.1 )
+    @minimum_delay_secs = minimum_delay_secs
+    @semaphore = Mutex.new
+    @next_call = nil      # Earliest time a new block can be executed
+  end
+
+
+  # Ensure that calls do not exceed the rate limit by delaying thread
+  # We assume that we must wait at least @minimum_delay_seconds since end of last call
+  # Note that the delay is measured from the end of any previous call
+  def throttle!
+    raise ArgumentError, "Must supply a block to throttle!" unless block_given?
+    @semaphore.synchronize do
+      unless @next_call.nil?
+        while Time.now < @next_call
+          doze_time = @next_call - Time.now
+          sleep doze_time
+        end
+      end
+      yield # Do the stuff
+      @next_call = Time.now + @minimum_delay_secs
+    end
+  end
+end
---
+++ test/throttler_test.rb
@@ -0,0 +1,65 @@
+require "test_helper"
+
+class ThrottlerTest < Minitest::Test
+
+  CALLS = 5
+  THREADS = 3
+  MINIMUM_DELAY_SECS = 0.5
+
+  SETUP = begin
+    puts "Expected Test Duration: ~#{CALLS * THREADS * MINIMUM_DELAY_SECS} secs"
+  end
+
+
+  def test_that_it_has_a_version_number
+    refute_nil ::Throttler::VERSION
+  end
+
+  # We try and replicate a multi-threaded set of calls
+  def test_throttling
+
+    throttler = Throttler.new( MINIMUM_DELAY_SECS )
+
+    threads = []
+    times = []    # Array of [Start, finish, thread number] for each job processed
+
+    # Execute THREADS*CALLS calls in muliple threads
+    (1..THREADS).each do |iThread|
+      threads << Thread.new {
+        (1..CALLS).each do |i|
+          throttler.throttle! do
+            start = Time.now
+            sleep [ 0, 0.1, 0.3, 1.1].sample
+            finish = Time.now
+            times << [start,finish,iThread]
+          end
+        end
+      }
+    end
+
+    threads.each { |thread| thread.join }
+
+    assert_equal THREADS*CALLS, times.length
+
+    # Check that all the executions were separated by MINIMUM_DELAY_SECS 
+    (1..times.length-1).each do |i|
+      start,finish,thread = times[i]
+      if false
+        puts "#{start.strftime('%H:%M:%S.%N')} - #{finish.strftime('%H:%M:%S.%N')} worked on thread #{thread}"
+      end
+      unless i==0
+        _, last_finish, _ = times[i-1]
+        delay = (start - last_finish)
+        assert delay >= MINIMUM_DELAY_SECS, "Delay between finish #{_pt(last_finish)} and start #{_pt(start)} was less than #{MINIMUM_DELAY_SECS}"
+      end
+    end
+
+
+    nil 
+  end
+
+
+  def _pt( time )
+    time.strftime("%H:%M:%S.%N")
+  end
+end
